18dd2056fc690bdbf266deb0d8b74212814caa9e
Eralp Karaduman <eralpkaraduman@gmail.com>
2019-05-31 19:10:20 +0300

    Added Google Maps key config for iOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"YOUR_GOOGLE_MAPS_KEY_HERE"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
